ffb90b61609ada2220fa0bc2b0844000a4a9de55
Ria Iyer <riyer4@ucsc.edu>
2025-01-29 20:06:36 -0800

    Final 4/4

--- src/Play.js
+++ src/Play.js
@@ -191,7 +191,7 @@ class Play extends Phaser.Scene {
 CODE CHALLENGE
 Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
 [X] Add ball reset logic on successful shot
-[ ] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
+[X] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
 [X] Make one obstacle move left/right and bounce against screen edges
 [X] Create and display shot counter, score, and successful shot percentage
 */
